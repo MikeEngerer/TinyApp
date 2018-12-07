baf05d91fe36d8f9f98a2a4e0a8df30bfd3eb246
MikeEngerer <michael.engerer@hotmail.com>
2018-12-07 21:06:01 +0000

    refactored urlDB to include ability to lookup owner of urls

--- urlDB.js
+++ urlDB.js
@@ -1,7 +1,14 @@
+var userDB = require("./userDB")
 
-
-module.exports = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+module.exports = urls = {
+	"b2xVn2": {
+		"longURL": "http://www.lighthouselabs.ca",
+		"user_id": userDB["user2RandomID"].id
+	},
+	"9sm5xK": {
+		"longURL": "http://www.google.com",
+		"user_id": userDB["userRandomID"].id
+	}
 };
+
 
